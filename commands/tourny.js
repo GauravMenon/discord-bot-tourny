09b7daedda8e023fc7a10388b6372172fe3b56d0
Gaurav Menon <gauravmenon37@gmail.com>
2024-04-11 21:41:27 -0600

    Refactoring and following the discord.js documentation. Created the ping, server and user.js files. Refactored the index.js file so it works with the new files

--- commands/tourny.js
+++ commands/tourny.js
@@ -1,6 +1,8 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder().setName('createtournament').setDescription('Creates the tournament that you want to use');
+export const data = new SlashCommandBuilder()
+    .setName('createtournament')
+    .setDescription('Creates the tournament that you want to use');
 
 export async function execute(interaction) {
     await interaction.reply('The tournament has been made! Good luck players!')
--- index.js
+++ index.js
@@ -1,36 +1,49 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import * as tourny from './commands/tourny.js'
 import * as quickmatch from './commands/quickmatch.js'
 
 config()
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Create a new client instance
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+client.commands = new Collection();
 
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// When the client is ready, run this code (only once)
-client.once(Events.ClientReady, readyDiscord);
-
-// Login to Discord with the client's token
 client.login(process.env.TOKEN);
 
-client.on(Events.InteractionCreate, handleInteraction)
 
-function readyDiscord() {
-    console.log('👋 ' + client.user.tag)
-}
 
-async function handleInteraction(interaction) {
-    if (!interaction.isCommand())
-        return;
-    if (interaction.commandName === 'createtournament') {
-        await tourny.execute(interaction)
-    }
-    if (interaction.commandName === 'quickmatch'){
-        await quickmatch.execute(interaction)
-    }
-}
+// // Create a new client instance
+// const client = new Client({
+//     intents: [GatewayIntentBits.Guilds],
+
+// });
+
+// // When the client is ready, run this code (only once)
+// client.once(Events.ClientReady, readyDiscord);
+
+// // Login to Discord with the client's token
+// client.login(process.env.TOKEN);
+
+// client.on(Events.InteractionCreate, handleInteraction)
+
+// function readyDiscord() {
+//     console.log('👋 ' + client.user.tag)
+// }
+
+// async function handleInteraction(interaction) {
+//     if (!interaction.isCommand())
+//         return;
+//     if (interaction.commandName === 'createtournament') {
+//         await tourny.execute(interaction)
+//     }
+//     if (interaction.commandName === 'quickmatch'){
+//         await quickmatch.execute(interaction)
+//     }
+// }
