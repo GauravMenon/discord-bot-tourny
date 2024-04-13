1dfee141507b72798132aab9155a6d441ed2255c
Gaurav Menon <gauravmenon37@gmail.com>
2024-04-13 00:23:19 -0600

    Deleted old code in index.js

--- index.js
+++ index.js
@@ -40,34 +40,3 @@ for (const folder of commandFolder) {
 }
 
 client.login(process.env.TOKEN);
-
-
-
-// // Create a new client instance
-// const client = new Client({
-//     intents: [GatewayIntentBits.Guilds],
-
-// });
-
-// // When the client is ready, run this code (only once)
-// client.once(Events.ClientReady, readyDiscord);
-
-// // Login to Discord with the client's token
-// client.login(process.env.TOKEN);
-
-// client.on(Events.InteractionCreate, handleInteraction)
-
-// function readyDiscord() {
-//     console.log('👋 ' + client.user.tag)
-// }
-
-// async function handleInteraction(interaction) {
-//     if (!interaction.isCommand())
-//         return;
-//     if (interaction.commandName === 'createtournament') {
-//         await tourny.execute(interaction)
-//     }
-//     if (interaction.commandName === 'quickmatch'){
-//         await quickmatch.execute(interaction)
-//     }
-// }
